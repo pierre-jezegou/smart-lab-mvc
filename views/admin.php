47a932fb29a8f2c1c1c4eda194204ce1a99c1d11
Saraaaahh <93844006+Saraaaahh@users.noreply.github.com>
2023-02-13 17:48:43 +0100

    Update admin.php

--- views/admin.php
+++ views/admin.php
@@ -4,22 +4,22 @@
 <section class="accueil">
 <h1>Page administrateur</h1>
     <div class="links">  
-        <a class="inscription" href="admin/register">
-            <span class="material-symbols-outlined">person_add</span>
-            <div class="link-title">Nouveau compte</div>
-            <div class="description">Créer ou modifier un compte d'accès pour les utilisateurs</div>
+        <a class="inscription" href="admin/register"> <!-- Code le bloc Nouveau Compte -->
+            <span class="material-symbols-outlined">person_add</span> <!-- Icône -->
+            <div class="link-title">Nouveau compte</div> <!-- Titre -->
+            <div class="description">Créer ou modifier un compte d'accès pour les utilisateurs</div> <!-- Description -->
         </a>
-        <a class="actions" href="statistics">
+        <a class="actions" href="statistics"> <!-- Code le bloc Statistiques -->
             <span class="material-symbols-outlined">query_stats</span>
             <div class="link-title">Statistiques</div>
             <div class="description">Vue d'ensemble des opérations des utilisateurs</div>
         </a>
-        <a class="dossiers" href="index">
+        <a class="dossiers" href="index"> <!-- Code le bloc Suivi des Actions -->
             <span class="material-symbols-outlined">summarize</span>
             <div class="link-title">Suivi des actions</div>
             <div class="description">Accès aux dossiers patients</div>
         </a>
-        <a class="monitoring" href="monitoring">
+        <a class="monitoring" href="monitoring"> <!-- Code le monitoring -->
             <span class="material-symbols-outlined">monitoring</span>
             <div class="link-title">Monitoring</div>
             <div class="description">Etat de la connexion au logiciel Sillage</div>
